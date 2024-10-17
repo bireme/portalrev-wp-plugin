484b3df84b2003c145c925930f3baae45050f2dd
do Carmo <docarmgab@paho.org>
2024-10-17 16:43:43 -0300

    Update cluster.php

--- template/cluster.php
+++ template/cluster.php
@@ -4,6 +4,7 @@ ini_set('display_errors', '0');
 
 $lang = $_POST['lang'];
 $site_lang = $_POST['site_lang'];
+$lang = substr($site_lang,0,2);
 $query = stripslashes($_POST['query']);
 $filter = stripslashes($_POST['filter']);
 $user_filter = stripslashes($_POST['uf']);
@@ -13,7 +14,7 @@ $cluster_fb = ( $_POST['cluster'] ) ? $_POST['cluster'].':'.$fb : '';
 $count = 1;
 
 
-$cc_service_request = $cc_service_url . 'api/title/search/?q=' . urlencode($query) . '&fq=' . urlencode($filter) . '&fb=' . $cluster_fb . '&lang=' . $site_lang . '&count=' . $count;
+$cc_service_request = $cc_service_url . 'api/title/search/?q=' . urlencode($query) . '&fq=' . urlencode($filter) . '&fb=' . $cluster_fb . '&lang=' . $lang . '&count=' . $count;
 
 //echo "<pre>"; echo " | "; echo($cc_service_request); echo "</pre>"; die();
 //echo $cc_service_request;
