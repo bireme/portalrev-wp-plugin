c1207a95c0b8b20c6587a6f89b79924e31341918
do Carmo <docarmgab@paho.org>
2024-10-23 09:41:59 -0300

    Correcao cluster
    Correcao cluster

--- template/cluster.php
+++ template/cluster.php
@@ -17,7 +17,7 @@ $count = 1;
 $cc_service_request = $cc_service_url . 'api/title/search/?q=' . urlencode($query) . '&fq=' . urlencode($filter) . '&fb=' . $cluster_fb . '&lang=' . $lang . '&count=' . $count;
 
 //echo "<pre>"; echo " | "; echo($cc_service_request); echo "</pre>"; die();
-echo $cc_service_request;
+//echo $cc_service_request;
 $response = @file_get_contents($cc_service_request);
 if ($response){
     $response_json = json_decode($response);
